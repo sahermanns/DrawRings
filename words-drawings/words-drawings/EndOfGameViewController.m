615e98367b91ea6478a0f8577bba96baa768c543
Sarah Hermanns <sahermanns@gmail.com>
2015-09-21 16:21:14 -0700

    added FB share button

--- words-drawings/words-drawings/EndOfGameViewController.m
+++ words-drawings/words-drawings/EndOfGameViewController.m
@@ -21,29 +21,9 @@
     [super viewDidLoad];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)shareToFB:(id)sender {
-  
-//  [FacebookLogin LoginServiceForFacebook:^(ACAccount *) {
-//    
-//  }];
-  
 
-  
   if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
     
     SLComposeViewController *fbSheetOBJ = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
@@ -53,6 +33,6 @@
     [self presentViewController:fbSheetOBJ animated:YES completion:nil];
   }
   
-  
 }
+
 @end
